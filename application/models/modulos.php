6bab6711a31c56359add7a4436711109b16bbe01
Neriberto Caetano do Prado <neriberto@gmail.com>
2012-01-04 17:29:16 -0200

    redesign menu

--- application/models/modulos.php
+++ application/models/modulos.php
@@ -34,14 +34,14 @@ class modulos extends CI_Model {
 	
 	function menu() {
 		?>
-		<div class="ui-widget-header ui-corner-all" style="margin-top: 20px; padding-top: 0px; padding-right: 0.7em; padding-bottom: 0px; padding-left: 0.7em;">
+		<div id="menu" class="ui-widget-header ui-corner-all" style="margin-top: 20px; padding-top: 0px; padding-right: 0.7em; padding-bottom: 0px; padding-left: 0.7em;">
     <p>
-        <a href="<?php echo site_url();?>">Principal | </a>
-        <a href="<?php echo site_url('servidores');?>">Servidores | </a>
+        <a href="<?php echo site_url();?>">Principal</a> | 
+        <a href="<?php echo site_url('servidores');?>">Servidores</a> | 
 		<span>SquidGuard { </span>
 	    <a href="<?php echo site_url('categorias');?>">Categorias</a>
 	    <a href="<?php echo site_url('domain');?>">Domínios</a>
-	    <a href="<?php echo site_url('urls');?>">URLs }</a>
+	    <a href="<?php echo site_url('urls');?>">URLs</a> }
 </p> 
 </div><?php
 }
